de15d896050b2a7aa733d442ef796e9d3a6497ed
Noctua <mannygozzidev@protonmail.com>
2022-10-18 21:53:06 -0700

    Starting files

---
+++ GameLoop.js
@@ -0,0 +1,7 @@
+import Constants from "./Constants";
+
+const GameLoop = (entities, { touches, dispatch, events}) => {
+    return entities;
+}
+
+export {GameLoop};
--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 import { registerRootComponent } from 'expo';
-
+import React, { Component } from 'react'
+import { View, StyleSheet } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
+import {GameEngine} from 'react-native-game-engine'
+import Constants from './Constants';
+import Head from './Head';
+import {GameLoop} from './GameLoop'
+export default class Snake extends Component {
+    constructor(props) {
+        super(props);
+        this.boardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+        this.engine = null;
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <GameEngine
+                    ref={(ref) => { this.engine = ref }}
+                    style={{ width: this.boardSize, height: this.boardSize, flex: null, backgroundColor: '#ffffff' }}
+                    systems={[GameLoop]}
+                    entities={{
+                        head: { position: [0, 0], size: Constants.CELL_SIZE, renderer: <Head /> }
+                    }}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(appName, () => Snake);
